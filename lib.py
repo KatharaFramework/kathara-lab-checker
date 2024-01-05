115c3cb1acdbc831b4f4548c8f739aceddb5102b
Lorenzo <lorenzoariemma@gmail.com>
2024-01-05 16:35:20 +0100

    Fix crash if DNS server refuse to answer

--- lib.py
+++ lib.py
@@ -19,8 +19,8 @@ def get_output(exec_output):
     try:
         while True:
             (stdout, stderr) = next(exec_output)
-            stdout = stdout.decode('utf-8') if stdout else ""
-            stderr = stderr.decode('utf-8') if stderr else ""
+            stdout = stdout.decode("utf-8") if stdout else ""
+            stderr = stderr.decode("utf-8") if stderr else ""
 
             if stdout:
                 output += stdout
@@ -56,8 +56,10 @@ def check_collision_domain(cd_t: Link, lab: Lab) -> tuple[str, bool, str]:
     try:
         cd = lab.get_link(cd_t.name)
         if cd.machines.keys() != cd_t.machines.keys():
-            reason = (f"Devices connected to collision domain {cd.name} {list(cd.machines.keys())} "
-                      f"are different from the one in the template {list(cd_t.machines.keys())}.")
+            reason = (
+                f"Devices connected to collision domain {cd.name} {list(cd.machines.keys())} "
+                f"are different from the one in the template {list(cd_t.machines.keys())}."
+            )
             logger.log_red(reason)
             return test_text, False, reason
         logger.log_green("OK")
@@ -79,11 +81,9 @@ def check_running_daemon(device_name: str, daemon: str, lab: Lab) -> tuple[str, 
     try:
         device = lab.get_machine(device_name)
         logger.log(test_text, end="")
-        output = get_output(kathara_manager.exec(
-            machine_name=device.name,
-            lab_hash=lab.hash,
-            command=f"pgrep {daemon}"
-        ))
+        output = get_output(
+            kathara_manager.exec(machine_name=device.name, lab_hash=lab.hash, command=f"pgrep {daemon}")
+        )
         if output != "":
             logger.log_green("OK")
             return test_text, True, "OK"
@@ -97,20 +97,23 @@ def check_running_daemon(device_name: str, daemon: str, lab: Lab) -> tuple[str, 
 
 
 def get_kernel_routes(device: Machine, lab: Lab) -> dict[str, Any]:
-    output = get_output(kathara_manager.exec(
-        machine_name=device.name,
-        lab_hash=lab.hash,
-        command="ip -j route"
-    ))
+    output = get_output(
+        kathara_manager.exec(machine_name=device.name, lab_hash=lab.hash, command="ip -j route")
+    )
     return json.loads(output)
 
 
-def check_negative_route(device_name: str, route_to_check: str, next_hop: str, routes) -> tuple[str, bool, str]:
-    test_text = f"Check that route {route_to_check} " + (f"with nexthop {next_hop} " if next_hop else "") + \
-                f"IS NOT in the routing table of device `{device_name}`:\t"
+def check_negative_route(
+    device_name: str, route_to_check: str, next_hop: str, routes
+) -> tuple[str, bool, str]:
+    test_text = (
+        f"Check that route {route_to_check} "
+        + (f"with nexthop {next_hop} " if next_hop else "")
+        + f"IS NOT in the routing table of device `{device_name}`:\t"
+    )
     logger.log(test_text, end="")
     for route in routes:
-        if route['dst'] == route_to_check:
+        if route["dst"] == route_to_check:
             reason = f"The route `{route_to_check}` IS in the routing table!"
             logger.log_red(reason)
             return test_text, False, reason
@@ -118,12 +121,17 @@ def check_negative_route(device_name: str, route_to_check: str, next_hop: str, r
     return test_text, True, "OK"
 
 
-def check_positive_route(device_name: str, route_to_check: str, next_hop: str, routes) -> tuple[str, bool, str]:
-    test_text = f"Check that route {route_to_check} " + (f"with nexthop {next_hop} " if next_hop else "") + \
-                f"IS in the routing table of device `{device_name}`:\t"
+def check_positive_route(
+    device_name: str, route_to_check: str, next_hop: str, routes
+) -> tuple[str, bool, str]:
+    test_text = (
+        f"Check that route {route_to_check} "
+        + (f"with nexthop {next_hop} " if next_hop else "")
+        + f"IS in the routing table of device `{device_name}`:\t"
+    )
     logger.log(test_text, end="")
     for route in routes:
-        if route['dst'] == route_to_check:
+        if route["dst"] == route_to_check:
             if next_hop:
                 if route["gateway"] == next_hop:
                     logger.log_green("OK")
@@ -167,15 +175,15 @@ def check_kernel_routes(device_name: str, routes_to_check: list[Union[str, tuple
 def check_bgp_peering(device: Machine, lab: Lab, neighbor: str) -> tuple[str, bool, str]:
     test_text = f"{device.name} has bgp peer {neighbor}:\t"
     logger.log(test_text, end="")
-    exec_output_gen = kathara_manager.exec(machine_name=device.name,
-                                           command="vtysh -e 'show ip bgp summary json'",
-                                           lab_hash=lab.hash)
+    exec_output_gen = kathara_manager.exec(
+        machine_name=device.name, command="vtysh -e 'show ip bgp summary json'", lab_hash=lab.hash
+    )
     output = get_output(exec_output_gen)
-    if output.startswith('ERROR:') or 'exec failed' in output:
+    if output.startswith("ERROR:") or "exec failed" in output:
         logger.log_red(output)
         return test_text, False, output
     output = json.loads(output)
-    for peer in output['ipv4Unicast']['peers']:
+    for peer in output["ipv4Unicast"]["peers"]:
         if neighbor == peer:
             logger.log_green("OK")
             return test_text, True, "OK"
@@ -187,10 +195,10 @@ def check_bgp_peering(device: Machine, lab: Lab, neighbor: str) -> tuple[str, bo
 def check_bgp_network_command(device: Machine, network: str, lab: Lab) -> tuple[str, bool, str]:
     test_text = f"Checking bgp network ({network}) for {device.name}:\t"
     logger.log(test_text, end="")
-    exec_output_gen = kathara_manager.exec(machine_name=device.name,
-                                           command="vtysh -e 'show running-config bgp'",
-                                           lab_hash=lab.hash)
-    output = list(filter(lambda x: "network" in x, get_output(exec_output_gen).split('\n')))
+    exec_output_gen = kathara_manager.exec(
+        machine_name=device.name, command="vtysh -e 'show running-config bgp'", lab_hash=lab.hash
+    )
+    output = list(filter(lambda x: "network" in x, get_output(exec_output_gen).split("\n")))
     for line in output:
         if re.search(rf"\s*network\s*{network}", line):
             logger.log_green("OK")
@@ -200,13 +208,16 @@ def check_bgp_network_command(device: Machine, network: str, lab: Lab) -> tuple[
     return test_text, False, reason
 
 
-def check_protocol_injection(device: Machine, protocol_to_check: str,
-                             injected_protocol: str, lab: Lab) -> tuple[str, bool, str]:
+def check_protocol_injection(
+    device: Machine, protocol_to_check: str, injected_protocol: str, lab: Lab
+) -> tuple[str, bool, str]:
     test_text = f"Checking that {injected_protocol} is injected into {protocol_to_check} for {device.name}:\t"
     logger.log(test_text, end="")
-    exec_output_gen = kathara_manager.exec(machine_name=device.name,
-                                           command=f"vtysh -e 'show running-config {protocol_to_check}'",
-                                           lab_hash=lab.hash)
+    exec_output_gen = kathara_manager.exec(
+        machine_name=device.name,
+        command=f"vtysh -e 'show running-config {protocol_to_check}'",
+        lab_hash=lab.hash,
+    )
     output = get_output(exec_output_gen).split("\n")
     for line in output:
         if re.search(rf"^\s*redistribute\s*{injected_protocol}$", line):
@@ -217,27 +228,28 @@ def check_protocol_injection(device: Machine, protocol_to_check: str,
     return test_text, False, reason
 
 
-def check_dns_authority_for_domain(domain: str, authority_ip: str, device_name: str, lab: Lab) -> tuple[str, bool, str]:
+def check_dns_authority_for_domain(
+    domain: str, authority_ip: str, device_name: str, lab: Lab
+) -> tuple[str, bool, str]:
     test_text = f"Checking that `{authority_ip}` is the authority for domain `{domain}`:\t"
     logger.log(test_text, end="")
-    exec_output_gen = kathara_manager.exec(machine_name=device_name,
-                                           command=f"dig NS {domain}",
-                                           lab_hash=lab.hash)
+    exec_output_gen = kathara_manager.exec(
+        machine_name=device_name, command=f"dig NS {domain}", lab_hash=lab.hash
+    )
 
     output = get_output(exec_output_gen)
-    if output.startswith('ERROR:'):
-        logger.log_red('\n' + output)
+    if output.startswith("ERROR:"):
+        logger.log_red("\n" + output)
         return test_text, False, output
-    result = jc.parse('dig', output)
-    if result:
+    result = jc.parse("dig", output)
+    if result and result[0]["status"] == "NOERROR":
         result = result.pop()
-
-        root_servers = list(map(lambda x: x['data'].split(" ")[0], result['answer']))
+        root_servers = list(map(lambda x: x["data"].split(" ")[0], result["answer"]))
         authority_ips = []
         for root_server in root_servers:
-            exec_output_gen = kathara_manager.exec(machine_name=device_name,
-                                                   command=f"dig +short {root_server}",
-                                                   lab_hash=lab.hash)
+            exec_output_gen = kathara_manager.exec(
+                machine_name=device_name, command=f"dig +short {root_server}", lab_hash=lab.hash
+            )
             ip = get_output(exec_output_gen).strip()
             if authority_ip == ip:
                 logger.log_green("OK")
@@ -248,19 +260,19 @@ def check_dns_authority_for_domain(domain: str, authority_ip: str, device_name: 
         logger.log_red(reason)
         return test_text, False, reason
     else:
-        logger.log_red('\n' + output)
+        logger.log_red("\n" + output)
         return test_text, False, output
 
 
 def check_local_name_server_for_device(local_ns_ip: str, device_name: str, lab: Lab) -> tuple[str, bool, str]:
     test_text = f"Checking that `{local_ns_ip}` is the local name server for device `{device_name}`:\t"
     logger.log(test_text, end="")
-    exec_output_gen = kathara_manager.exec(machine_name=device_name,
-                                           command=f"cat /etc/resolv.conf",
-                                           lab_hash=lab.hash)
+    exec_output_gen = kathara_manager.exec(
+        machine_name=device_name, command=f"cat /etc/resolv.conf", lab_hash=lab.hash
+    )
     output = get_output(exec_output_gen)
-    if output.startswith('ERROR:'):
-        logger.log_red('\n' + output)
+    if output.startswith("ERROR:"):
+        logger.log_red("\n" + output)
         return test_text, False, output
 
     lines = output.splitlines()
@@ -282,14 +294,16 @@ def verifying_reachability_from_device(device_name: str, destination: str, lab: 
     test_text = f"Verifying `{destination}` reachability from device `{device_name}`:\t"
     logger.log(test_text, end="")
     try:
-        exec_output_gen = kathara_manager.exec(machine_name=device_name,
-                                               command=f"bash -c 'ping -q -n -c 1 {destination}; echo $?'",
-                                               lab_hash=lab.hash)
+        exec_output_gen = kathara_manager.exec(
+            machine_name=device_name,
+            command=f"bash -c 'ping -q -n -c 1 {destination}; echo $?'",
+            lab_hash=lab.hash,
+        )
     except Exception as e:
         logger.log_red(str(e))
         return test_text, False, str(e)
     output = get_output(exec_output_gen)
-    if output.splitlines()[-1] == '0':
+    if output.splitlines()[-1] == "0":
         logger.log_green("OK")
         return test_text, True, "OK"
     else:
