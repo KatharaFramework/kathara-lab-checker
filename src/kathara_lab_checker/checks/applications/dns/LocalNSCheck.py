562ac1e9bce5f67bba1e0f19995cb316ea908cb0
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2024-10-29 17:20:06 +0100

    Fix LocalNSCheck

--- src/kathara_lab_checker/checks/applications/dns/LocalNSCheck.py
+++ src/kathara_lab_checker/checks/applications/dns/LocalNSCheck.py
@@ -26,18 +26,18 @@ class LocalNSCheck(AbstractCheck):
         if not lines:
             reason = f"`resolv.conf` file not found for device `{device_name}`"
             return CheckResult(self.description, False, reason)
+        actual_ips = []
         for line in lines:
             match = re.search(rf"^nameserver (.*)$", line)
             if match:
                 actual_ns_ip = match.group(1)
                 if actual_ns_ip == local_ns_ip:
                     return CheckResult(self.description, True, "OK")
-                else:
-                    reason = f"The local name server for device `{device_name}` has ip `{actual_ns_ip}`"
-                    return CheckResult(self.description, False, reason)
-            else:
-                reason = f"`resolv.conf` file has the wrong format for device `{device_name}`"
-                return CheckResult(self.description, False, reason)
+                actual_ips.append(actual_ns_ip)
+
+        reason = (f"There is no local name server for device `{device_name}` with IP `{local_ns_ip}`. "
+                  f"Actual nameservers: {actual_ips}")
+        return CheckResult(self.description, False, reason)
     def run(self, local_nameservers_to_devices: dict[str, list[str]], lab: Lab) -> list[CheckResult]:
         results = []
         for local_ns, managed_devices in local_nameservers_to_devices.items():
